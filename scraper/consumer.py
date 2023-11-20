4fe78c5142d9868589cec6fc70de1b17e6cff5af
Tarun Annapareddy <tarunannapareddy@rgntgw1-3-1-dhcp.int.colorado.edu>
2023-11-20 13:55:34 -0700

    consumer

--- scraper/consumer.py
+++ scraper/consumer.py
@@ -2,7 +2,7 @@ import json
 from kafka import KafkaConsumer
 
 BROKER = '10.142.0.6:9092'
-CONSUMER_TOPIC = 'quickstart-events'
+CONSUMER_TOPIC = 'events'
 
 if __name__ == '__main__':
     # Kafka Consumer 
@@ -12,4 +12,11 @@ if __name__ == '__main__':
         auto_offset_reset='earliest'
     )
     for message in consumer:
-        print(json.loads(message.value))
+        if message.value:  # Check if message is not empty
+            try:
+                payload = json.loads(message.value)
+                print(payload)
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON: {e}")
+        else:
+            print("Received an empty message")
