ffa9332358177a1b2f87b5027d21766aa1d6675c
Tarun Annapareddy <tarunannapareddy@Taruns-MacBook-Air.local>
2023-11-20 00:08:38 -0700

    change keys

--- scraper/script.py
+++ scraper/script.py
@@ -1,44 +1,23 @@
 from __future__ import print_function
 import os.path
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+from google.oauth2 import service_account
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '16IQ9G3f3YRUTa1MCeZBnq2km81qwR-GOkgO8yIYgcTU'
 SAMPLE_RANGE_NAME = 'Sheet1!A1:B101'
+SERVICE_ACCOUNT_FILE = 'keys.json'
+credentails = None
+credentials = service_account.Credentials.from_service_account_file( SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
 # Output file to write the data
 OUTPUT_FILE = 'output.txt'
 
 def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet and write to a local file.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'client_secret.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
     try:
-        service = build('sheets', 'v4', credentials=creds)
+        service = build('sheets', 'v4', credentials=credentials)
 
         # Call the Sheets API
         sheet = service.spreadsheets()
@@ -50,7 +29,6 @@ def main():
             print('No data found.')
             return
 
-        print('Name, Major:')
         with open(OUTPUT_FILE, 'w') as output_file:
             for row in values:
                 print(row)
