bcdd30bca96e86f622e92a72754df9d925d64902
Tarun Annapareddy <tarunannapareddy@Taruns-MacBook-Air.local>
2023-11-24 18:49:04 -0700

    setup redis

--- redis/redisworker.py
+++ redis/redisworker.py
@@ -8,18 +8,15 @@ redis_port = 6379
 r = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
 
 if __name__ == '__main__':
-    key =0
+    key = 0
     while(True):
-        key = key+1
-        time = datetime.now()
-        print(f"inserting data key {key} and value {time}")
-        r.set(key, time, px= 90000)
+        key +=1
+        value = datetime.now()
+        print("inserting data key {} and value {}".format(key, value))
+        r.set(str(key), str(value), px= 9000)
 
         print("printing all data")
-        r.set('')
         keys = r.keys()
-        for key in keys:
-           print(f'key {key} value {r.get(key)}')
-        time.sleep(30)
-        
-
+        for k in keys:
+           print(f'key {k} value {r.get(k)}')
+        time.sleep(3)
