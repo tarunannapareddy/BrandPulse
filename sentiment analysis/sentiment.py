f895ad5a5f3df9c7ac78566bce954995b93e9238
Akshay Bakshi <apbakshi@hotmail.com>
2023-12-01 22:31:51 -0700

    updated sentiment code

--- sentiment analysis/sentiment.py
+++ sentiment analysis/sentiment.py
@@ -1,4 +1,16 @@
-#! /usr/bin/python
+import os
+
+# Transformers
+transformers_path = 'transformers-4.35.2.tar.gz'
+os.system(f"pip install --no-index --find-links=./ {transformers_path}")
+
+# Torch
+torch_path = 'torch-2.1.1-cp311-cp311-manylinux2014_aarch64.whl'
+os.system(f"pip install --no-index --find-links=./ {torch_path}")
+
+# Numpy
+numpy_path = 'numpy-1.26.2.tar.gz'
+os.system(f"pip install --no-index --find-links=./ {numpy_path}")
 
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import Tokenizer
@@ -7,6 +19,7 @@ from pyspark.sql.functions import udf
 from pyspark.sql.types import ArrayType, FloatType, StringType
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
+import numpy as np
 
 # Create a Spark session
 spark = SparkSession.builder.appName("SentimentAnalysis").getOrCreate()
@@ -38,20 +51,12 @@ def categorize_sentiment(score):
     elif score >= 4:
         return 'Positive'
 
-# Define a UDF to apply the sentiment scoring function to Spark DataFrame
 sentiment_score_udf = udf(sentiment_score)
 
 # Apply sentiment scoring to the DataFrame
 df = df.withColumn("sentiment_score", sentiment_score_udf(df["text"]))
-
-# Define a UDF to apply the sentiment categorization function to Spark DataFrame
 sentiment_category_udf = udf(categorize_sentiment, StringType())
-
-# Apply sentiment categorization to the DataFrame
 df = df.withColumn("sentiment_category", sentiment_category_udf(df["sentiment_score"]))
-
-# Show the results
 df.select("text", "sentiment_score", "sentiment_category").show(truncate=False)
 
-# Stop the Spark session
 spark.stop()
